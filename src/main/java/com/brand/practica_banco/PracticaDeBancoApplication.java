ad2580eeb6c860728cca33953342d30f43b2d716
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-21 11:26:45 -0600

    fix: resolver problema con version maven

--- src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
+++ src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
@@ -1,5 +1,6 @@
 package com.brand.practica_banco;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
