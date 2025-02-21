23fca0f96d5bbd63204ecce11d4ae43ab561fe7b
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-21 10:55:15 -0600

    Creando Proyecto

---
+++ src/test/java/com/brand/practica_banco/PracticaDeBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brand.practica_banco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDeBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
