860546e508744b23930243686b4f7aef9d5a952c
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-21 22:43:38 -0600

    refactor: Agrega texto al inicio del main
    Agrega "Hola mundo" con logger y println

--- src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
+++ src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
@@ -1,15 +1,25 @@
 package com.brand.practica_banco;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class PracticaDeBancoApplication {
 
+	//Crear un logger
+	private static final Logger logger = LoggerFactory.getLogger(PracticaDeBancoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaDeBancoApplication.class, args);
-		
+		logger.info("Hola mundo con logger");
+		System.out.println("Hola mundo con println");
+
 	}
 
 }
