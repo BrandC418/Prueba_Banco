7c29b39ffe222324709bb9704a8ed9eeecb369e0
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-24 16:54:55 -0600

    Add CuentaDto

---
+++ src/main/java/com/brand/practica_banco/Entity/CuentaDto.java
@@ -0,0 +1,46 @@
+package com.brand.practica_banco.Entity;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CuentaDto {
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío.")
+    @Size(max = 100, message = "El nombre no puede tener más de 100 caracteres.")
+    private String nombre;
+
+    @NotNull(message = "El saldo no puede ser nulo.")
+    @DecimalMin(value = "0.0", inclusive = true, message = "El saldo no puede ser negativo.")
+    @DecimalMax(value = "10000000.00", message = "El saldo no puede exceder $10,000,000.00")
+    private BigDecimal saldo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
