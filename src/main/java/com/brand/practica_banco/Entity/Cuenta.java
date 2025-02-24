378ce49841eb1b7fa93693a0051eb21bde2112c2
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-23 23:28:03 -0600

    refactor(cuenta): Improve the Account entity and add auditing

--- src/main/java/com/brand/practica_banco/Entity/Cuenta.java
+++ src/main/java/com/brand/practica_banco/Entity/Cuenta.java
@@ -1,46 +1,52 @@
 package com.brand.practica_banco.Entity;
-
+import jakarta.validation.constraints.*;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "cuentas")
-@Data
+@EntityListeners(AuditingEntityListener.class)  //Para auditorias
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
 public class Cuenta {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false, length = 100, unique = true)
+    @NotBlank(message = "El nombre no puede estar vacío.")
+    @Size(max = 100, message = "El nombre no puede tener mas de 100 caracteres.")
     private String nombre;
 
-    @Column
+    @Column(nullable = false, precision = 15, scale = 2)
+    @NotNull(message = "El saldo no puede ser nulo.")
+    @DecimalMin(value = "0.0", inclusive = true, message = "El saldo no puede ser negativo.")
+    @DecimalMax(value = "10000000.00", message = "El saldo no puede exceder $10,000,000.00")
     private BigDecimal saldo;
 
-    public Long getId() {
-        return id;
-    }
+    @CreatedDate
+    @Column(name = "fecha_creacion", updatable = false)
+    private LocalDateTime fechaCreacion;
 
-    public void setId(Long id) {
+    @LastModifiedDate
+    @Column(name = "fecha_actualizacion")
+    private LocalDateTime fechaActualizacion;
+
+    @Builder
+    public Cuenta(Long id, String nombre, BigDecimal saldo){
         this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
+        //Los campos de auditoria (fechaCreacion, fechaActualizacion) NO se incluye aquí.
     }
 }
