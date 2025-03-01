2290fe0c2285f7bfdee8c355f4e65bed2267f3f1
BrandC418 <brandon.adolfo.p.c@gmail.com>
2025-02-28 18:52:59 -0600

    AddSwaggerAndConfig

--- src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
+++ src/main/java/com/brand/practica_banco/PracticaDeBancoApplication.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
